520782c0d571cfc7c488f7f3eecbdfccdc3c6c57
Shahir Taj <shahir.taj@gmail.com>
2020-10-21 10:44:07 -0500

    Add files via upload

--- compare_paragraphs.py
+++ compare_paragraphs.py
@@ -1,3 +1,10 @@
+"""
+# Name(s): Shahir Taj
+# Course: CSCI 4000 - A
+# Date: 10/21/2020
+
+"""
+
 import tensorflow_hub as hub
 import numpy as np
 import tensorflow_text
@@ -15,20 +22,25 @@ def compare_paragraphs(segment_one, segment_two):
     # Compute similarity matrix. Higher score indicates greater similarity.
     similarity_matrix = np.inner(segment_one_result, segment_two_result)
 
-    print(similarity_matrix)
     return similarity_matrix
 
 
-paragraph_one = ["What do we know of this death? In part, it is a death that does not know itself; it is the living "
-                 "death of those who believe themselves living. But in part, also, it is a death that is suffered: "
-                 "'When the commandment came, sin revived and I died' (Rom. 7:9-10). What shall we say? Without doubt "
-                 "it is legitimate to compare this death that is suffered with the experience of division and conflict "
-                 "described in the pericope of the Epistle to the Romans (7:14—19), which follows the dialectic of sin "
-                 "and the law reported above. Death, then, is the actualized dualism of the Spirit and the flesh."]
-paragraph_two = ["Wherefore the law is holy, and the commandment holy, and just, and good, Was then that which is good "
-                 "made death unto me? God forbid. But sin, that it might appear sin, working death in me by that which "
-                 "is good; that sin by the commandment might become exceeding sinful."]
+def main():
+    # We could also do some kind of comparison between the individual sentences in the query and segment.
+    paragraph_one = ["What do we know of this death? In part, it is a death that does not know itself; it is the living"
+                     " death of those who believe themselves living. But in part, also, it is a death that is suffered:"
+                     " 'When the commandment came, sin revived and I died' (Rom. 7:9-10). What shall we say? Without "
+                     "doubt it is legitimate to compare this death that is suffered with the experience of division and"
+                     " conflict described in the pericope of the Epistle to the Romans (7:14—19), which follows the "
+                     "dialectic of sin and the law reported above. Death, then, is the actualized dualism of the Spirit"
+                     " and the flesh."]
+    paragraph_two = ["Wherefore the law is holy, and the commandment holy, and just, and good, Was then that which is "
+                     "good made death unto me? God forbid. But sin, that it might appear sin, working death in me by "
+                     "that which is good; that sin by the commandment might become exceeding sinful."]
 
-# We could also do some kind of comparison between the individual sentences in the query and segment.
+    proximity = compare_paragraphs(paragraph_one, paragraph_two)
+    print(proximity)
 
-compare_paragraphs(paragraph_one, paragraph_two)
+
+if __name__ == "__main__":
+    main()
