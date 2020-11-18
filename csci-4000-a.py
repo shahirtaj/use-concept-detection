276a0dc06b16f1323d64f8cb4918285daadf8fc0
Shahir Taj <shahir.taj@gmail.com>
2020-11-18 12:05:40 -0600

    Add files via upload

--- csci-4000-a.py
+++ csci-4000-a.py
@@ -1,7 +1,7 @@
 """
 # Name(s): Shahir Taj
 # Course: CSCI 4000 - A
-# Date: 11/11/2020
+# Date: 11/18/2020
 
 """
 
@@ -9,6 +9,7 @@ import tensorflow_hub as hub
 import numpy as np
 import tensorflow_text
 import glob
+import pandas as pd
 
 
 def get_article(filename):
@@ -31,16 +32,16 @@ def get_article(filename):
 
 
 def calculate_proximities(embed, articles, num_paragraphs):
-    paragraph_proximities = np.empty((num_paragraphs, num_paragraphs))
+    proximities = np.empty((num_paragraphs, num_paragraphs))
 
     # Create a matrix of the proximities between all paragraphs.
     for i, (target_paragraph_key, target_paragraph) in enumerate(articles.items()):
         for j, (compared_paragraph_key, compared_paragraph) in enumerate(articles.items()):
             # Calculate the proximity between each paragraph in all articles.
             proximity = compare_paragraphs(embed, target_paragraph, compared_paragraph)
-            paragraph_proximities[i, j] = proximity
+            proximities[i, j] = proximity
 
-    return paragraph_proximities
+    return proximities
 
 
 def compare_paragraphs(embed, segment_one, segment_two):
@@ -54,6 +55,13 @@ def compare_paragraphs(embed, segment_one, segment_two):
     similarity_matrix = np.inner(segment_one_result, segment_two_result)
 
     return similarity_matrix
+
+
+def write_csv(data, labels):
+    dataframe = pd.DataFrame(data)
+    dataframe.columns = labels
+    dataframe.index = labels
+    dataframe.to_csv("output/paragraph_proximities.csv")
 
 
 def main():
@@ -71,20 +79,20 @@ def main():
         exit()
 
     articles = {}
+    paragraph_labels = []
     num_paragraphs = 0
     # Create a dictionary mapping paragraph locations to paragraph text.
-    for i, input_file in enumerate(input_files):
-        article = get_article(input_file)
+    for file in input_files:
+        article = get_article(file)
         for j, paragraph in enumerate(article):
-            # Define paragraph locations as (article index, paragraph index).
-            articles[(i, j)] = paragraph
-        num_paragraphs += len(article)
+            articles[(file[6:9], j)] = paragraph
+            paragraph_labels.append(file[6:9] + ", " + str(j))
+            num_paragraphs += 1
 
     embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder-multilingual-large/3")
     paragraph_proximities = calculate_proximities(embed, articles, num_paragraphs)
 
-    # Save paragraph_proximities as csv file
-    # Maintain first three characters of filename for table labels
+    write_csv(paragraph_proximities, paragraph_labels)
 
 
 if __name__ == "__main__":
